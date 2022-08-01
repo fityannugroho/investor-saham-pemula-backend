472aaa34c72b5da58e5bf67b981e1dcc9e3f90f4
Fityan <fityannugroho@gmail.com>
2022-08-01 09:32:04 +0700

    Add `IsNotEmpty` decorator

--- src/users/dto/add-user.payload.ts
+++ src/users/dto/add-user.payload.ts
@@ -1,17 +1,27 @@
-import { IsEmail, IsString, Length, MaxLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Length,
+  MaxLength,
+} from 'class-validator';
 
 export class AddUserPayload {
+  @IsNotEmpty()
   @IsString()
   @MaxLength(255)
   name: string;
 
+  @IsNotEmpty()
   @IsEmail()
   @MaxLength(320)
   email: string;
 
+  @IsNotEmpty()
   @IsString()
   password: string;
 
+  @IsNotEmpty()
   @IsString()
   @Length(16, 16)
   roleId: string;
