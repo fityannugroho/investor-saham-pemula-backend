b347b346b2b90968f6da6d8184e75eceeb8ec688
Fityan <fityannugroho@gmail.com>
2022-08-05 14:04:59 +0700

    Remove `AdminsModule` import at `ArticlesModule
    - Delete `verifyAuthorIsAdmin()`.
    - Remove `AdminsService` injection in `ArticlesService`.

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { AdminsModule } from 'src/admins/admins.module';
 
 @Module({
-  imports: [PrismaModule, AdminsModule],
+  imports: [PrismaModule],
   controllers: [ArticlesController],
   providers: [ArticlesService],
   exports: [ArticlesService],
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,27 +1,11 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { nanoid } from 'nanoid';
-import { AdminsService } from 'src/admins/admins.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateArticlePayload } from './dto/create-article.payload';
 
 @Injectable()
 export class ArticlesService {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly adminsService: AdminsService,
-  ) {}
-
-  /**
-   * Verify that the author is an admin.
-   * @param authorId The author id.
-   */
-  async verifyAuthorIsAdmin(authorId: string): Promise<void> {
-    try {
-      await this.adminsService.getAdminById(authorId);
-    } catch {
-      throw new ForbiddenException('Author must be an admin');
-    }
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
   /**
    * Create new article.
