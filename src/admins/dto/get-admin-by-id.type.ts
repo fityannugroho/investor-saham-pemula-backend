ed73bd9bbf083f60fc6cbc83f7ae8bcab2942727
Fityan <fityannugroho@gmail.com>
2022-08-08 20:28:27 +0700

    Update `GetAdminByIdType`

--- src/admins/dto/get-admin-by-id.type.ts
+++ src/admins/dto/get-admin-by-id.type.ts
@@ -1,10 +1,3 @@
-import { PickType } from '@nestjs/mapped-types';
 import { AdminValidator } from './AdminValidator';
 
-class PickedAdmin extends PickType(AdminValidator, [
-  'id',
-  'name',
-  'email',
-] as const) {}
-
-export type GetAdminByIdType = PickedAdmin;
+export type GetAdminByIdType = Pick<AdminValidator, 'id' | 'name' | 'email'>;
