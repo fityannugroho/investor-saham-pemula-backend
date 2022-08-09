a9833ecb2e2d9381a0018d89cc6588049f5637b0
Fityan <fityannugroho@gmail.com>
2022-08-09 15:34:43 +0700

    Render the OpenAPI docs with `@nestjs/swagger`
    - Install `@nestjs/swagger` v6.0.5.
    - Use `GET /docs` to  render the docs from `api.json` file .

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -17,6 +19,10 @@ async function bootstrap() {
 
   // Validate all endpoints
   app.useGlobalPipes(new ValidationPipe());
+
+  // Render the API documentation
+  const swaggerDoc = JSON.parse(fs.readFileSync('api.json').toString());
+  SwaggerModule.setup('docs', app, swaggerDoc);
 
   // Start the server
   const host = process.env.HOST || '0.0.0.0';
