a60d323d23156baa7547010927a75ef595dee53b
Fityan <fityannugroho@gmail.com>
2022-08-14 21:31:38 +0700

    Add branch email verification

--- src/branches/branches.service.ts
+++ src/branches/branches.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { nanoid } from 'nanoid';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegistrantsService } from 'src/registrants/registrants.service';
@@ -12,11 +12,27 @@ export class BranchesService {
   ) {}
 
   /**
+   * Verify if the branch email is already registered.
+   * @param email The branch email.
+   * @throws {BadRequestException} If the branch email already exists.
+   */
+  async verifyBranchEmail(email: string): Promise<void> {
+    const branch = await this.prisma.branch.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+    if (branch) {
+      throw new BadRequestException(['Email already exists']);
+    }
+  }
+
+  /**
    * Add new branch.
    * @param data The branch data.
    * @returns The branch id.
    */
   async addBranch(data: AddBranchDataType): Promise<string> {
+    await this.verifyBranchEmail(data.email);
     // TODO: Validate the `registrantId` field.
 
     const id = nanoid(16);
