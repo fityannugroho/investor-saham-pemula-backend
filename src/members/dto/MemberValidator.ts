f42175f40bb825114c22ee3ffb5c42617cc82614
Fityan <fityannugroho@gmail.com>
2022-08-14 10:03:48 +0700

    Create `MemberValidator`

---
+++ src/members/dto/MemberValidator.ts
@@ -0,0 +1,62 @@
+import {
+  IsDateString,
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { EqualsAny } from 'src/common/decorator/EqualsAny';
+import { IsNanoid } from 'src/common/decorator/IsNanoid';
+import { IsNotSymbol } from 'src/common/decorator/IsNotSymbol';
+
+export class MemberValidator {
+  @IsNotEmpty()
+  @IsNanoid(16)
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsNotSymbol()
+  @MaxLength(255)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @MaxLength(320)
+  email: string;
+
+  @IsNotEmpty()
+  @EqualsAny(['male', 'female'])
+  gender: 'male' | 'female';
+
+  @IsNotEmpty()
+  @IsNumberString()
+  age: number;
+
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255)
+  socialMedia: string;
+
+  @IsOptional()
+  @IsDateString()
+  createdAt?: Date;
+
+  @IsOptional()
+  @IsDateString()
+  updatedAt?: Date;
+
+  @IsOptional()
+  @IsNanoid(16)
+  adminId: string;
+
+  @IsOptional()
+  @IsDateString()
+  acceptedAt?: Date;
+}
